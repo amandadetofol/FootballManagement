a6917839fb3c5c40e073a10e258c126bb10506a4
amandadetofol <amandadetofol@192.168.1.3>
2023-07-30 17:34:58 -0300

    create event factory

--- PerebasFC/PerebasFC/Features/NewEventInCalendar/Factory/NewEventInCalendarFactory.swift
+++ PerebasFC/PerebasFC/Features/NewEventInCalendar/Factory/NewEventInCalendarFactory.swift
@@ -5,4 +5,19 @@
 //  Created by Amanda Detofol on 26/07/23.
 //
 
-import Foundation
+import UIKit
+
+struct NewEventInCalendarFactory {
+    
+    static func getNewEventInCalendarViewController(navigationController: UINavigationController) -> NewEventInCalendarViewController {
+        let coordinator = NewEventInCalendarCoordinator(navigationController: navigationController)
+        let worker = NewEventInCalendarWorker()
+        let interactor = NewEventInCalendarInteractor(
+            coordinator: coordinator,
+            worker: worker)
+        let controller = NewEventInCalendarViewController(interactor: interactor)
+        
+        return controller
+    }
+    
+}
